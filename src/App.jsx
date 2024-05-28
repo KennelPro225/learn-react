850661117cc384e0431c7038858c56b04f8d973b
Kassi Georges Kennel <kennelkassi@gmail.com>
2024-05-28 20:43:35 +0000

    video 4: La syntaxe Jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const title = 'Bonjour les gens';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    alert("Click Here")
+  }
+  return <>
+    <h1 onClick={handleClick} id="title">{title}</h1>
+  </>
 }
 
 export default App
